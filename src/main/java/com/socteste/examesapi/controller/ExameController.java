a3ec50e2d231bfe53b4d60a6c232c421f0e840b8
Renan Tavares <renantavaresti@gmail.com>
2021-01-24 01:34:13 -0300

    ExameController Implementado

--- src/main/java/com/socteste/examesapi/controller/ExameController.java
+++ src/main/java/com/socteste/examesapi/controller/ExameController.java
@@ -2,19 +2,56 @@ package com.socteste.examesapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.socteste.examesapi.model.Exame;
+import com.socteste.examesapi.service.ExameService;
 
 @RestController
-@RequestMapping("/exames")
+@RequestMapping(value = "/exames")
 public class ExameController {
 
+	@Autowired
+	private ExameService ex;
+
+	@CrossOrigin
 	@GetMapping
-	public List<Exame> listar() {
-		return null;
+	public List<Exame> listarTodosExames() {
+		return ex.listarExame();
+	}
+
+	@CrossOrigin
+	@GetMapping("/{id}")
+	public Exame listarUnicoExame(@PathVariable(value = "id") Long id) {
+		return ex.listarUnicoExame(id);
+	}
+
+	@CrossOrigin
+	@PostMapping
+	public ResponseEntity<String> cadastarExame(@RequestBody Exame exame) {
+		return ex.cadastrarExame(exame);
+	}
+
+	@CrossOrigin
+	@PutMapping
+	public ResponseEntity<String> atualizarExame(@RequestBody Exame exame) {
+		return ex.atualizarExame(exame);
+	}
+
+	@CrossOrigin
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletarExame(@PathVariable(value = "id") Long id) {
+		return ex.deletarExame(id);
 	}
 
 }
